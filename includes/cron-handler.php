65de1436818bf2b4847fd7c0b33e508f2637bcd4
Abdur Rahman Bilal <aramb@aramservices.com>
2025-01-10 15:51:18 -0500

    Rename CRON handler function for clarity and consistency

--- includes/cron-handler.php
+++ includes/cron-handler.php
@@ -1,6 +1,6 @@
 <?php
 // Handle CRON job scheduling and execution
-function salah_cron_handler()
+function salah_cron_handler_include()
 {
     $options = get_option('salah_plugin_settings', ['fetch_days' => []]);
     $fetch_days = $options['fetch_days'];
