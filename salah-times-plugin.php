de10087c8b31ec0e7d693bfaffc8dceee6cc94dd
Abdur Rahman Bilal <aramb@aramservices.com>
2025-01-10 15:39:10 -0500

    Remove CRON job handling from Salah Times plugin; simplify code structure

--- salah-times-plugin.php
+++ salah-times-plugin.php
@@ -127,14 +127,3 @@ add_action('update_option_salah_plugin_settings', function ($old_value, $value) 
         salah_unschedule_cron();
     }
 }, 10, 2);
-
-// Handle CRON job
-add_action('salah_cron_job', 'salah_cron_handler');
-function salah_cron_handler()
-{
-    $options = get_option('salah_plugin_settings', ['fetch_days' => []]);
-    $fetch_days = $options['fetch_days'];
-    if (in_array(date('w'), $fetch_days)) {
-        salah_fetch_api();
-    }
-}
